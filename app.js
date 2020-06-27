3d84717b72e575ba20e01c27352cbdf2ab2d3372
Aryakrishnacr <48838111+Aryakrishnacr@users.noreply.github.com>
2020-06-27 23:52:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const express=require("express");
+const bodyParser = require('body-parser')
+const nav=[
+    {
+        link:'/books',name:'books'},
+{
+    link:'/authors',name:'authors'},
+{
+        link:'/signup',name:'signup'},
+    {
+            link:'/login',name:'login'},
+     {
+            link:'/books',name:'add book'},
+    
+     {
+            link:'/author',name:'add author'}
+    
+
+
+];
+
+const booksRouter=require("./src/routes/booksRoutes")(nav)
+const authorsRouter=require("./src/routes/authorsRoutes")(nav)
+const signupRouter=require("./src/routes/signupRoutes")(nav)
+const loginRouter=require("./src/routes/loginRoutes")(nav)
+const adminRouter=require("./src/routes/adminRoutes")(nav)
+const addauthorRouter=require("./src/routes/addauthorRoutes")(nav)
+
+const app=new express();
+app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static('./public'));
+app.use("/books",booksRouter);
+app.use("/authors",authorsRouter);
+app.use("/signup",signupRouter);
+app.use("/login",loginRouter);
+app.use("/admin",adminRouter);
+app.use("/author",addauthorRouter);
+
+app.set('view engine','ejs');
+app.set('views','./src/views');
+app.get('/',function(req,res){
+    res.render("index",{
+    nav,
+    title:'library'
+});
+});
+
+
+app.listen(5000);
